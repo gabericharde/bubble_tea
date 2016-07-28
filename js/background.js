dfcf9ecd21f88058fbe4eb92501d07e74697f1c5
Gabriel RiCharde <gabe.richarde@my.wheaton.edu>
2016-07-28 09:29:54 +0200

    Removed "global: true" from description of keyboard commands and tweaks to background.js

--- js/background.js
+++ js/background.js
@@ -3,14 +3,15 @@
 // chrome.browserAction.onClicked(
 //	);
 
-chrome.commands.getAll(function(commands){
-  console.log(commands)
-})
+//chrome.commands.getAll(function(commands){
+//  console.log(commands)
+//})
 
 chrome.commands.onCommand.addListener(function(command) {
-  if(command === "bubble-tea-command") {
-    var pageTitle = document.head.getElementsByTagName("title").innerHTML[0];
-    pageTitle;
-	console.log(pageTitle);
+  if(command == "bubble-tea-command") {
+  	chrome.extension.getBackgroundPage().console.log(2+2);
+  	alert("Your bubble-tea-command fired."); // for testing purposes only
+    var pageTitle = document.title;
+    console.log(pageTitle);
   };
 });
